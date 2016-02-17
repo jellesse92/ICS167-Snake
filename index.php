64243d90e57a865f59b8afb5a2eaf0da7a981b72
jellesse92 <jehunter@uci.edu>
2016-02-16 21:01:46 -0800

    Adding Client Files from Milestone1

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<!-- 
+Jasmine Hunter
+Arielle Chongco
+Jayson Gan
+Akihiro 
+
+
+ -->
+<title> Homework 1 - Pong Game</title>
+</head>
+<h2>Snake! </h2>
+<div id="total_score"><h4>Current Score: <div id="score">0</div></h4></div>
+<table width="300" height = "300"><div id ="gameboard"></div></table>
+<div id="totalsomething"></div>
+
+<body>
+<form id="User1"></form>
+</body>
+<script type="text/javascript">
+$(document).ready(function(){
+	document.getElementByID("totalsomething").innerHTML= "NO";
+	});
+</script>
+</html>
+
+
+	
